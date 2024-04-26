09475749a3f01331d7d617abcf06231f15a26ae3
aarushiv16 <102585082+aarushiv16@users.noreply.github.com>
2024-04-26 13:13:40 +0530

    Add files via upload

---
+++ 1 - Getting Started/4 - Control Flow/Lab - Control Flow.playground/Pages/8. Exercise - Ternary Operator.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+/*:
+## Exercise - Ternary Operator
+ 
+ Refactor the code below so that `largest` is declared and assigned to in one line using the ternary operator.
+ */
+let number1 = 24
+let number2 = 30
+
+var largest: Int
+largest = number1 > number2 ? number1 : number2
+print(largest)
+/*:
+[Previous](@previous)  |  page 8 of 9  |  [Next: App Exercise - Ternary Messages](@next)
+ */
